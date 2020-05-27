bca2fd45877a734dc8b3137b877f6076752028ee
anshganatra <ansh.ganatra@somaiya.edu>
2020-05-28 02:54:40 +0530

    firestore message fetching successful

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -59,6 +59,8 @@ class ChatScreen extends StatelessWidget {
     },
   ];
 
+  final messageTextController = TextEditingController();
+  String messageText;
 
   @override
   Widget build(BuildContext context) {
@@ -90,58 +92,88 @@ class ChatScreen extends StatelessWidget {
           ],
         ),
         backgroundColor: Colors.grey[200],
-        body: ListView.builder(
-          itemCount: randomMessages.length,
-          itemBuilder: (BuildContext context, int index) {
-            return MessageBubble(
-              isMe: randomMessages[index]['isMe'],
-              sender: randomMessages[index]['sender'],
-              text: randomMessages[index]['text'],
-            );
-          },
+        // body: ListView.builder(
+        //   itemCount: randomMessages.length,
+        //   itemBuilder: (BuildContext context, int index) {
+        //     return MessageBubble(
+        //       isMe: randomMessages[index]['isMe'],
+        //       sender: randomMessages[index]['sender'],
+        //       text: randomMessages[index]['text'],
+        //     );
+        //   },
+        // ),
+        body: Column(
+          children: <Widget>[
+            MessagesStream(),
+            Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      controller: messageTextController,
+                      onChanged: (value) {
+                        messageText = value;
+                      },
+                      style: kTextItLoudHeadingStyle.copyWith(fontSize:16.0),
+                    ),
+                  ),
+                  FlatButton(
+                    onPressed: () {
+                      messageTextController.clear();
+                      _firestore.collection('sessions').document('sessionid1').collection('listOfMessages').add({
+                        'text': messageText,
+                        'sender': sessionUsername,
+                      });
+                    },
+                    child: Text(
+                      'Send',
+                      style: kTextItLoudHeadingStyle.copyWith(fontSize:16.0),
+                    ),
+                  ),
+                ],
+              ),
+          ],
         ),
       ),
     );
   }
 }
 
-// class MessagesStream extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return StreamBuilder<DocumentSnapshot>(
-//       stream: _firestore.collection('messages').document('sessionid1').snapshots(),
-//       builder: (context, snapshot) {
-//         if (!snapshot.hasData) {
-//           return Center(
-//             child: CircularProgressIndicator(
-//               backgroundColor: Colors.lightBlueAccent,
-//             ),
-//           );
-//         }
-//         final messages = snapshot.data.documents.reversed;
-//         List<MessageBubble> messageBubbles = [];
-//         for (var message in messages) {
-//           final messageText = message.data['text'];
-//           final messageSender = message.data['sender'];
+class MessagesStream extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _firestore.collection('sessions').document('sessionid1').collection('listOfMessages').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.lightBlueAccent,
+            ),
+          );
+        }
+        final messages = snapshot.data.documents.reversed;
+        List<MessageBubble> messageBubbles = [];
+        for (var message in messages) {
+          final messageText = message.data['text'];
+          final messageSender = message.data['sender'];
 
-//           final currentUser = loggedInUser.email;
+          final messageBubble = MessageBubble(
+            sender: messageSender,
+            text: messageText,
+            isMe: sessionUsername == messageSender,
+          );
 
-//           final messageBubble = MessageBubble(
-//             sender: messageSender,
-//             text: messageText,
-//             isMe: currentUser == messageSender,
-//           );
-
-//           messageBubbles.add(messageBubble);
-//         }
-//         return Expanded(
-//           child: ListView(
-//             reverse: true,
-//             padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-//             children: messageBubbles,
-//           ),
-//         );
-//       },
-//     );
-//   }
-// }
+          messageBubbles.add(messageBubble);
+        }
+        return Expanded(
+                  child: ListView(
+            reverse: true,
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+            children: messageBubbles,
+          ),
+        );
+      },
+    );
+  }
+}
